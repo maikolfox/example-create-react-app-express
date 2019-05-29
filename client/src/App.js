eb70d1fe334f43960a14a447ac9d695694dbeb73
Maikol AHOUE <maikol.ahoue@bridgebankgroup.com>
2019-05-29 13:46:13 +0000

    add backend server to front

--- client/src/App.js
+++ client/src/App.js
@@ -1,24 +1,35 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-  state = { response: '', post: '', responseToPost: '', };
+ 
+  state = {
+     response: '', 
+     task: '', 
+     status:'',
+     responseToPost: '', 
+    };
   componentDidMount() { this.callApi().then(res => this.setState({ response: res.express })).catch(err => console.log(err)); }
   callApi = async () => {
-    const response = await fetch('/api/hello'); const body = await response.json();
+    const response = await fetch('/tasks'); const body = await response.json();
     if (response.status !== 200) throw Error(body.message);
     return body;
   };
   handleSubmit = async e => {
     e.preventDefault(); 
-    const response = await fetch('/api/world', 
+    const response = await fetch('/tasks', 
                     { method: 'POST', 
                       headers: 
                       { 
                         'Content-Type': 'application/json', 
                       }, 
-                      body: JSON.stringify({ post: this.state.post }), }); const body = await response.text();
+                      body: JSON.stringify(
+                        { 
+                          task: this.state.task, 
+                          status:this.state.status
+                        }), 
+                      }); 
+                      const body = await response.text();
 
                       this.setState({ responseToPost: body });  
                     
@@ -27,15 +38,19 @@ class App extends Component {
       return (      
       <div className="App">        
         <header className="App-header">          
-          <img src={logo} className="App-logo" alt="logo" />          
-          <p>Edit <code>src/App.js</code> and save to reload.</p>
-          <a  className="App-link" href="https://reactjs.org" target="_blank" rel="noopener noreferrer">Learn React</a>
-        </header>        
+          
           <p>{this.state.response}</p><form onSubmit={this.handleSubmit}><p><strong>Post to Server:</strong></p>
-          <input type="text" value={this.state.post} onChange={e => this.setState({ post: e.target.value })}/>
+          <label for="cheese">Task</label>
+          <input type="text" id="taskname" value={this.state.task} onChange={e => this.setState({ task: e.target.value })}/>
+            &nbsp;&nbsp;&nbsp;
+          <label for="cheese">Status</label>
+          <input type="text" id="taskstatus" value={this.state.status} onChange={e => this.setState({ status: e.target.value })}/>
           <button type="submit">Submit</button></form>
           <p>{this.state.responseToPost}</p>
-          </div>);  
+          </header>        
+
+          </div>
+          );  
           }
 
 
--- server.js
+++ server.js
@@ -1,7 +1,27 @@
-const express = require('express'); const bodyParser = require('body-parser');
-const app = express(); const port = process.env.PORT || 5000; 
-app.use(bodyParser.json()); 
-app.use(bodyParser.urlencoded({ extended: true })); 
-app.get('/api/hello', (req, res) => { res.send({ express: 'Hello From Express' }); });
-app.post('/api/world', (req, res) => { console.log(req.body); res.send(`I received your POST request. This is what you sent me: ${req.body.post}`); });
-app.listen(port, () => console.log(`Listening on port ${port}`));
+const express = require('express'),
+  app = express(),
+  bodyParser = require('body-parser');
+  port = process.env.PORT || 5000;
+
+
+const mysql = require('mysql');
+// connection configurations
+const mc = mysql.createConnection({
+    host: 'localhost',
+    user: 'root',
+    password: 'root',
+    database: 'mydb'
+});
+ 
+// connect to database
+mc.connect();+
+
+app.listen(port);
+
+console.log('API server started on: ' + port);
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+var routes = require('./app/routes/appRoutes'); //importing route
+routes(app); //register the route
